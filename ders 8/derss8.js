19c1e4e9e021b130d29ca06fa77cabca7950644c
Nihad512 <73600990+Nihad512@users.noreply.github.com>
2020-11-23 16:13:50 +0400

    Add files via upload

---
+++ ders 8/derss8.js
@@ -0,0 +1,12 @@
+function notmyfunction(){
+    document.getElementById('drop').style='display:block;';
+}
+function not(){
+    document.getElementById('drop').style='display:none;';
+}
+function show(){
+    document.getElementById('search').style='display:block;';
+}
+function hide(){
+    document.getElementById('search').style='display:none;';
+}
